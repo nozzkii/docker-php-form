81685b847b9ba460fa75872f6fe3f4a96627f4cb
Anhtai <anhtai.thai@hotmail.de>
2020-11-28 00:13:36 +0100

    corrected sentence in received.php

--- src/received.php
+++ src/received.php
@@ -5,6 +5,6 @@ header("Location: index.php");
 include "db/database-con.php";
 
 if(!empty($_POST["firstName"])&& $_POST["submit"]){
-    $_SESSION["Message"] = "Thank " . $_POST["firstName"] . " for sending us a message";
+    $_SESSION["Message"] = "Thank you " . $_POST["firstName"] . " for sending us a message";
     $customDB->sendData();
 }
